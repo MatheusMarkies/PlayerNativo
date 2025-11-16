8a97f078a5178a269270cadc6a5d50b7d8e86dc6
Matheus Markies <48259420+MatheusMarkies@users.noreply.github.com>
2025-11-16 08:33:29 -0300

    Init

--- app/src/main/java/com/brasens/playernativo/VideoSurfaceView.kt
+++ app/src/main/java/com/brasens/playernativo/VideoSurfaceView.kt
@@ -9,8 +9,6 @@ class VideoSurfaceView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyle: Int = 0
 ) : SurfaceView(context, attrs, defStyle) {
-
-    // Defina o aspect ratio do seu vídeo (1920x1080 = 16:9)
     private val VIDEO_ASPECT_RATIO = 16.0 / 9.0
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -28,13 +26,9 @@ class VideoSurfaceView @JvmOverloads constructor(
         val viewAspectRatio = width.toDouble() / height
 
         if (viewAspectRatio > VIDEO_ASPECT_RATIO) {
-            // A View é mais LARGA que o vídeo (ex: 1340x600)
-            // Corrija a altura para manter o aspect ratio do vídeo
             newHeight = (width / VIDEO_ASPECT_RATIO).toInt()
             newWidth = width
         } else {
-            // A View é mais ALTA que o vídeo
-            // Corrija a largura para manter o aspect ratio do vídeo
             newWidth = (height * VIDEO_ASPECT_RATIO).toInt()
             newHeight = height
         }
